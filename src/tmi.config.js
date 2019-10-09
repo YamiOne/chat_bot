93b35b8d1938c06f9624e0c704b6ddd24616d251
Emir Maljanovic <emir.maljanovic@gmail.com>
2019-10-09 20:12:31 +0200

    Remove

--- src/tmi.config.js
+++ src/tmi.config.js
@@ -7,7 +7,7 @@ const config = {
   },
   identity: {
     username: 'emoteanalyzer',
-    password: 'oauth:f5urbt22gz0b9j41z03bawu0d6ciua' // need auth token
+    password: '' // need auth token
   },
   channels : []
 };
